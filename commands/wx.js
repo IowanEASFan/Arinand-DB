6c5b0b3a86625aaf18abe5627c0864dff3c73066
Ferry Corsten  Marshmello <46287209+MarshmelloSUCKS@users.noreply.github.com>
2019-05-25 13:21:48 -0500

    Fixed the weather command

--- commands/wx.js
+++ commands/wx.js
@@ -4,24 +4,29 @@ var weather = require('weather-js');
 
 module.exports.run = async (bot, message, args) => {
 	weather.find({search: args.join(" "), degreeType: 'F'}, function(err, result) {
-		if(err) console.log(err);
-	   
-		var current = result[0].current;
-		var location = result[0].location;
-		const embed = new Discord.RichEmbed()
-		.setAuthor(`Weather for ${current.observationpoint}`)
-		.setTitle(`**${current.skytext}**`)
-		.setThumbnail(current.imageUrl)
-		.setColor('#1e74ff')
-		.addField('Current temperature:', `${current.temperature}°`, true)
-		.addField('Timezone:', `UTC${location.timezone}`, true)
-		.addField("Feels Like:", `${current.feelslike}°`, true)
-		.addField("Wind:", current.winddisplay, true)
-		.addField("°F or °C:", `°${location.degreetype}`, true)
-		.addField("Humidity:", `${current.humidity}%`, true);
+		
+		try {
+			var current = result[0].current;
+			var location = result[0].location;
+			
+			const embed = new Discord.RichEmbed()
+				.setAuthor(`Weather for ${current.observationpoint}`)
+				.setTitle(`**${current.skytext}**`)
+				.setThumbnail(current.imageUrl)
+				.setColor('#1e74ff')
+				.addField('Current temperature:', `${current.temperature}°`, true)
+				.addField('Timezone:', `UTC${location.timezone}`, true)
+				.addField("Feels Like:", `${current.feelslike}°`, true)
+				.addField("Wind:", current.winddisplay, true)
+				.addField("°F or °C:", `°${location.degreetype}`, true)
+				.addField("Humidity:", `${current.humidity}%`, true);
 
-		message.channel.send((embed))
-
+			message.channel.send((embed))
+		}
+		  
+		catch(err) {
+			return message.channel.send ("`Sorry, but that place doesn't exist!`");
+		}
         
     });
 }
