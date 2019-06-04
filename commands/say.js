afc2cc0e1cecd189ea383f3c7619c6a277721c34
Ferry Corsten  Marshmello <46287209+MarshmelloSUCKS@users.noreply.github.com>
2019-06-04 17:06:27 -0500

    Update to the `a-say` command
    All users can now use the a-say command. Arinand does display who sent the command.

--- commands/say.js
+++ commands/say.js
@@ -2,10 +2,16 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 
-if(message.author.id !== '254804060652175361') return;
 let botmessage = args.join(" ");
 message.delete().catch();
-message.channel.send(botmessage);
+
+let sayembed = new Discord.RichEmbed()
+.setAuthor(`${message.author.tag} says...`)
+.setThumbnail("https://i.ibb.co/cYFyyds/download-2.png")
+.setColor('#0099ff')
+.setDescription(botmessage);
+
+message.channel.send(sayembed);
 
 }
 module.exports.help = {
