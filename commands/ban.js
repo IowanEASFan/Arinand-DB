1900175ae1ac258ec78b7aca13b4b0d865101762
Ferry Corsten  Marshmello <46287209+MarshmelloSUCKS@users.noreply.github.com>
2019-06-12 12:38:15 -0500

    Update to `ban.js`
    You can now ban users without having a channel named `#reports`!

--- commands/ban.js
+++ commands/ban.js
@@ -2,15 +2,17 @@ const Discord = require("discord.js")
 
 module.exports.run = async (bot, message, args) => {
 let bannedUser = message.guild.member(message.mentions.users.first() || message.guild.members.get(args[0]));
-		if(!bannedUser) return message.channel.send("```Error 404 - User Not Found```");
-		let BanReason = args.join(" ").slice(22);
-        if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Unable to ban member.")
-		if(bannedUser.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Sorry, but this user has the **MANAGE_MESSAGES** role, therefore they are unbanable.")
+	if(!bannedUser) return message.channel.send("```Error 404 - User Not Found```");
+	
+	let BanReason = args.join(" ").slice(22);
+	
+	if(!message.member.hasPermission("BAN_MEMBERS")) return message.channel.send("Unable to ban member.")
+	if(bannedUser.hasPermission('MANAGE_MESSAGES')) return message.channel.send("Sorry, but this user has the **MANAGE_MESSAGES** role, therefore they are unbanable.")
 		if (!BanReason) {
 		        BanReason = "Unstated";
 		    }
 
-		let banEmbed = new Discord.RichEmbed()
+	let banEmbed = new Discord.RichEmbed()
 		.addField("User was banned!", `Log created at ${message.createdAt}`)
 		.setColor("#FF0000")
 		.addField("Banned User:", `${bannedUser}, ${bannedUser.id}`)
@@ -18,10 +20,11 @@ let bannedUser = message.guild.member(message.mentions.users.first() || message.
 		.addField("Reason for ban:", BanReason);
 
 		let banChannel = message.guild.channels.find('name', 'reports')
-		if(!banChannel) return message.channel.send('Error: channel named **reports** not found!')
+		if(!banChannel) message.guild.member(bannedUser).ban(BanReason) + 
+		message.channel.send (`User was banned by command of **${message.author.tag}** due to "**${BanReason}**".`);
 
-message.guild.member(bannedUser).ban(BanReason);
-banChannel.send(banEmbed);
+	message.guild.member(bannedUser).ban(BanReason);
+	if(banChannel) banChannel.send(banEmbed);
 
 	return;
 }
