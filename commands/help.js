2b85f3d46e364e88d0d62705c1f83bcdcf4bb1b2
Ferry Corsten  Marshmello <46287209+MarshmelloSUCKS@users.noreply.github.com>
2019-06-14 16:50:47 -0500

    Update help.js

--- commands/help.js
+++ commands/help.js
@@ -1,18 +1,27 @@
-const Discord = require("discord.js");
+const Discord = require('discord.js');
 
 module.exports.run = async (bot, message, args) => {
-
-	let help = new Discord.RichEmbed()
-    .setColor("#1e74ff")
+    let userhelp = new Discord.RichEmbed()
+    .setColor('#0099ff')
     .setThumbnail('https://i.ibb.co/mC2Ny48/arinand-av.png')
-	.addField("Arinand's Commands", "Need help? Here it is!")
-	.addField("a-userhelp", "Shows user commands.")
-	.addField("a-staffhelp", "Shows staff commands.")
-
-return message.channel.send(help);
-
+    .addField("Arinand's User Commands", "Need help? Here it is!")
+    .addField("a-userhelp", "Ya just did it.")
+    .addField("a-8ball", "it's an 8ball command, duh")
+    .addField("a-botinfo", "Displays the information of Arinand.")
+    .addField("a-cat", "Displays a random image of a cat.")
+    .addField("a-coin", "Tosses a coin.")
+    .addField("a-dice", "Rolls a die.")
+    .addField("a-dog", "Displays a random image of a dog.")
+    .addField("a-ha", "Shows a reactionary GIF to any joke you or others crack.")
+    .addField("a-ping", "Shows the response time between Arinand and Discord.")
+    .addField("a-rate", "Rates anything on a 1-to-5 star scale.")
+    .addField("a-serverinfo", "Displays the information of a guild.")
+    .addField("a-wx", "Shows weather information for a specific area.")
+    .addField("a-wx+c", "Shows weather information for a specific area with temperature information in Celcius.");
+    
+    message.channel.send(userhelp);
 }
 
 module.exports.help = {
-    name: "help"
+    name: 'userhelp' 
 }
