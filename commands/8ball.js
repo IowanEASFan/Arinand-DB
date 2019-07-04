a924c0f8f32393cf73cb9bace6343b084b8cfd75
Ferry Corsten  Marshmello <46287209+MarshmelloSUCKS@users.noreply.github.com>
2019-07-04 10:40:49 -0500

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -3,15 +3,15 @@ const Discord = require("discord.js");
 module.exports.run = async (bot, message, args) => {
 
 if(!args[2]) return message.reply("You didn't ask a proper question!");
-let replies = ["Hell yeah!", "Yeah!", "Yeah, I guess...", "Maybe.", "It's a 50/50 shot, bud.", "Nah.", "Hell nah.", "FUCK NO!"];
+let replies = ["It is certain.", "Without a doubt.", "Signs point to yes.", "Ask again later.", "Better not tell you now.", "Cannot predict now.", "Don't count on it.", "My reply is no.", "Very doubtful."];
 
 let result = Math.floor((Math.random() * replies.length));
-let question = args.slice(1).join(" ");
+let question = args.slice(0).join(" ");
 
 let ballembed = new Discord.RichEmbed()
 .setAuthor(message.author.tag)
 .setThumbnail("https://i.ibb.co/jkdchNc/download-2.jpg")
-.setColor("#1e74ff")
+.setColor('#0099ff')
 .addField("Question", question)
 .addField("Answer", replies[result]);
 
@@ -22,4 +22,3 @@ message.channel.send(ballembed);
 module.exports.help = {
     name: "8ball"
 }
-//
